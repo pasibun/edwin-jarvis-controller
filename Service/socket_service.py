01199317523545d7c7fc624f03c55e9553af8692
pasibun <matthijs_hovestad@hotmail.com>
2020-10-14 19:40:08 +0200

    testje met socket

--- Service/socket_service.py
+++ Service/socket_service.py
@@ -2,12 +2,10 @@ import socket
 
 
 class Socket(object):
-    HOST = '10.0.0.109'
+    HOST = '10.0.0.30'
     PORT = 1337
 
     def send_socket(self, input):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.HOST, self.PORT))
-            s.sendall(input)
-            data = s.recv(1024)
-        print('Received', repr(data))
+            s.sendall(input.encode())
