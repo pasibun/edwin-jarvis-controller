b5ace60a6295ce20cfb86152e75bcf298ebbcb0a
pasibun <matthijs_hovestad@hotmail.com>
2020-10-14 13:39:38 +0200

    ff loopen die zooi

--- edwin-jarvis-controller.py
+++ edwin-jarvis-controller.py
@@ -10,12 +10,13 @@ class EdwinJarvisController(object):
     def controller(self):
         value = self.control_board.keypad_input()
         print(value)
-        if value == ControlBoardTypes.RIGHT or value == ControlBoardTypes.LEFT:
-            self.mqtt.send_msg(self.mqtt.MQTT_TOPIC_BASE, value.name)
-        elif value == ControlBoardTypes.UP or value == ControlBoardTypes.DOWN:
-            self.mqtt.send_msg(self.mqtt.MQTT_TOPIC_FIRST_AXIS, value.name)
-        else:
-            self.mqtt.send_msg(self.mqtt.MQTT_TOPIC, value.name)
+        while True:
+            if value == ControlBoardTypes.RIGHT or value == ControlBoardTypes.LEFT:
+                self.mqtt.send_msg(self.mqtt.MQTT_TOPIC_BASE, value.name)
+            elif value == ControlBoardTypes.UP or value == ControlBoardTypes.DOWN:
+                self.mqtt.send_msg(self.mqtt.MQTT_TOPIC_FIRST_AXIS, value.name)
+            else:
+                self.mqtt.send_msg(self.mqtt.MQTT_TOPIC, value.name)
 
 
 if __name__ == "__main__":
